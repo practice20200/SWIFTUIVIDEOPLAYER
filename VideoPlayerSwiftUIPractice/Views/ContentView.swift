8e348a88d8ae9e7a0f3588bc92fa1d0d880a4a30
practice20200 <wor9hard2018@gmail.com>
2022-05-15 15:14:48 -0700

    Created tag

--- VideoPlayerSwiftUIPractice/Views/ContentView.swift
+++ VideoPlayerSwiftUIPractice/Views/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            HStack{
+                ForEach(Query.allCases, id: \.self){ searchQuery in
+                    QueryTag(query: searchQuery, isSelected: false)
+                }
+            }
+        }
+        .background(Color("AccentColor"))
     }
 }
 
